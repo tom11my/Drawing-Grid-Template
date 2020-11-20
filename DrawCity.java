e54a250a01f2bc95525b22093a7437acc4e8837e
Tommy Joseph <tommy11jo@gmail.com>
2020-11-20 03:04:24 -0800

    cosmetic cleaning

--- DrawCity.java
+++ DrawCity.java
@@ -25,8 +25,6 @@ public class DrawCity extends JFrame {
 		buildingBtn.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				//System.out.println("building button pressed");
-				//nullify previous clicks
 				canvas.isFirstClicked = false;
 				canvas.isSecondClicked = false;
 				
@@ -40,11 +38,8 @@ public class DrawCity extends JFrame {
 		startStreet.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				//System.out.println("road button pressed");
-				//nullify previous clicks
 				canvas.isFirstClicked = false;
 				canvas.isSecondClicked = false;
-				//create a button for user to terminate a street instance
 				startStreetPressed = !startStreetPressed;
 				buildingPressed = false;
 				endStreetPressed = false;
@@ -57,11 +52,8 @@ public class DrawCity extends JFrame {
 		endStreet.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				//System.out.println("road button pressed");
-				//nullify previous clicks
 				canvas.isFirstClicked = false;
 				canvas.isSecondClicked = false;
-				//create a button for user to terminate a street instance
 				endStreetPressed = !endStreetPressed;
 				buildingPressed = false;
 				startStreetPressed = false;
@@ -75,7 +67,6 @@ public class DrawCity extends JFrame {
 			public void actionPerformed(ActionEvent e) {
 				canvas.isSecondClicked = false;
 				canvas.isFirstClicked = false;
-				//canvas.isFirstClicked = canvas.getLatestObject().equals("Street") ? true: false;
 				canvas.fresh = true;
 				endStreetPressed = false;
 				buildingPressed = false;
@@ -85,9 +76,7 @@ public class DrawCity extends JFrame {
 		});
 		//Set up a custom drawing JPanel
 		canvas = new DrawCanvas();
-		//canvas.setPreferredSize(new Dimension(CANVAS_WIDTH, CANVAS_HEIGHT));
 		
-		//Add both panels to this JFrame's content-pane
 		cp = getContentPane();
 		cp.setLayout(new BorderLayout());
 		cp.add(canvas, BorderLayout.CENTER);
@@ -98,18 +87,15 @@ public class DrawCity extends JFrame {
 		pack();
 		setVisible(true);
 		
-		//set up grid of locations
 		int standWidth = CANVAS_WIDTH/squares[0].length;
 		int standHeight = CANVAS_HEIGHT/squares.length;
 		for(int row = 0; row < squares.length; row++) {
 			for(int col = 0; col < squares[0].length; col++) {
-				//preserves x y formatting
 				squares[row][col] = new Rectangle(new Vec2(col*standWidth, row*standHeight), standWidth, standHeight);
 			}
 		}
 	}
 	
-	//Define inner class DrawCanvas
 	class DrawCanvas extends JPanel implements MouseListener, MouseMotionListener, MouseWheelListener{
 		private PrintWriter printWriter;
 		
@@ -138,7 +124,6 @@ public class DrawCity extends JFrame {
 		private final Color STREET_COLOR = new Color(140, 0, 255);
 		private Color TEMP_STREET_COLOR = new Color(180, 180, 255);
 		private final Color TEMP_BUILDING_COLOR = Color.LIGHT_GRAY;
-		//new Color(0, 144, 201);
 		//boolean used to ensure that firstClick is not changed more than once before secondClick is found and the rectangle drawn
 		private Mat33 oldTransform = new Mat33();
 		private Mat33 currentTransform = new Mat33();
@@ -149,10 +134,7 @@ public class DrawCity extends JFrame {
 		}
 		@Override
 		public void paintComponent(Graphics g) {
-			//super.paintComponent(g);
-			//System.out.println("intersections " + intersections);
 			Graphics2D g2d = (Graphics2D) g;
-			//blank screen each time repaint() is internally called
 			g2d.setColor(Color.BLACK);
 			g2d.fillRect(0, 0,  CANVAS_WIDTH,  CANVAS_HEIGHT);
 			Vec3 scrollLocMod = scrollLoc.convertTo3D();
@@ -163,45 +145,33 @@ public class DrawCity extends JFrame {
 			 * 3. Translate back to original mouse loc
 			 * Through Matrices
 			 */
-			//set up currentTransform to apply on each Rectangle
-			//Because zoomFactor is most often 1.0 or -1.0, x^zoomFactor where x > 1 is a logical scalar
-			//LIMITATION: For any x where x is not a whole number, x^zoomFactor creates unequal spaces between squares
-			//NOTE: currentTransform is multiplied by oldTransform here
 			float scalar = (float)Math.pow(2, zoomFactor);
 			currentTransform = Mat33.findTransMat(shift.convertTo3D().getNeg()).multiply(Mat33.findTransMat(scrollLocMod.getNeg()).multiply(Mat33.findScaleMat(scalar).multiply((Mat33.findTransMat(scrollLocMod).multiply(oldTransform)))));
 			for(int row = 0; row < squares.length; row++) {
 				for(int col = 0; col < squares[0].length; col++) {
 					Rectangle s = squares[row][col];
-					//Transforming each individual point that makes up the Rectangle
 					Vec2[] points = s.getPoints();
 					for(int i = 0; i < 4; i++) {
 						points[i] = currentTransform.multipliedBy(points[i].convertTo3D()).trim();
 					}
 					s = new Rectangle(points[0], (points[1].getX() - points[0].getX()), (points[2].getY()-points[0].getY()), s.getColor());
 					
-					//left top corner of Rectangle
 					int xStart = (int)s.getLoc().getX();
 					int yStart = (int)s.getLoc().getY();
-					//must go outside the "onCanvas" loop below to allow real-time updates not based on mouseLoc
 					if (endStreetPressed) {
-						//startStreetPressed goes to false in action listener
 						removeTemporaryStreetColor();
 						streets.add(tempStreet);
-						//reset initial conditions
 						tempStreet = null;
+						
 						isFirstClicked = false;
 						isSecondClicked = false;
 						fresh = true;
-						//prevents excessive strain by only running once
 						endStreetPressed = false;
 					}
 					//(OnCanvas) - allows inner code to run when given square is on the canvas
 					if(xStart <= CANVAS_WIDTH && yStart <= CANVAS_HEIGHT && xStart >= -s.getWidth() && yStart >= -s.getHeight()) {
 						if(curLoc.getX() >= xStart && curLoc.getX() < xStart + s.getWidth() && curLoc.getY() >= yStart && curLoc.getY() < yStart + s.getHeight()) {
-							s.setColor(Color.RED);
-							//tracks clicks
-							
-							//creates building and applies its color based on clicks
+							s.setColor(Color.RED);							
 							if(buildingPressed) {
 								if(isFirstClicked && !isSecondClicked && fresh) {
 									firstClick = new Vec2(col, row);
@@ -230,39 +200,25 @@ public class DrawCity extends JFrame {
 									fresh = false;
 								}
 								else if(isFirstClicked && !isSecondClicked){
-									//refreshes street image based on changes to mouse location
-									//code here
-									removeTemporaryStreetColor();
-									/*ArrayList<Vec2> clonedList = (ArrayList<Vec2>)tempStreet.getNodes().clone();
-									clonedList.add(new Vec2(col, row));
-									Street cloned = new Street(clonedList);*/
-									
+									removeTemporaryStreetColor();									
 									applyTemporaryStreetColor(tempStreet, tempStreet.getNodes().get(tempStreet.getNodes().size()-1), new Vec2(col, row));
 								}
 								else if (isFirstClicked && isSecondClicked){
 									secondClick = new Vec2(col, row);
 									tempStreet.addNode(secondClick);
-									//applyStreetColor(tempStreet);
-									//System.out.println("reached only once");
-									//System.out.println(tempStreet.getNodes());
 									applyStreetColor(tempStreet, tempStreet.getNodes().get(tempStreet.getNodes().size()-2), secondClick);
 									isSecondClicked = false;
-									//tempStreet = null;
 								}
 							}
 						}
 						g2d.setColor(s.getColor());
-						//grid lines appear by shortening the borders of all Rectangles as they are printed
-						//logic accounts for zoom situation where all squares are 1 X 1 pixels
 						if((int)s.getWidth() != 1)
 							g2d.fillRect(xStart +1, yStart+1, (int)s.getWidth()-1, (int)s.getHeight()-1);
 						else g2d.fillRect(xStart, yStart, (int)s.getWidth(), (int)s.getHeight());
 					}
 				}
 			}
-			//set current transform to old transform for the next cycle
 			oldTransform = currentTransform;
-			//set variables applied to transform to 0
 			zoomFactor = 0;
 			shift = new Vec2(0, 0);
 			
@@ -329,11 +285,8 @@ public class DrawCity extends JFrame {
 			}
 		}
 		
-		//street related methods
 		//FLAW: Multiple connections between two line segments will create future confusion when a single car is traveling
-		//FIX: Redo the way intersections are created but with lines and exactness. RIP
 		public void applyStreetColor(Street s, Vec2 start, Vec2 end) {
-			//fixing intersection problem with previousNode
 			ArrayList<Vec2> toBeColored = s.findColoredLocsBetweenNodes(start, end);
 			Vec2 previous = toBeColored.get(0);
 			//prevents multiple "intersections" from being performed due to multiple overlaps in surrounding colored squares
@@ -346,12 +299,8 @@ public class DrawCity extends JFrame {
 				Vec2 loc = toBeColored.get(i);
 				int x = (int)loc.getX();
 				int y = (int)loc.getY();
-				//if(squares[y][x].getColor() == STREET_COLOR)
-				//Check if 8 squares surrounding a given square are STREET_COLOR. We call this an intersection.
 				Vec2 surrounding = checkSurroundingForColor(x, y, STREET_COLOR);
 				
-					
-				//verify that surrounding is not part of previous node connector (creates unintentional intersection)
 				boolean isPartOfPrevious = false;
 				for(Vec2 prev: previousNodes) {
 					System.out.println(surrounding);
@@ -360,14 +309,11 @@ public class DrawCity extends JFrame {
 				}
 				if(surrounding != null && !isPartOfPrevious &&(surrounding.getX() != previous.getX() || surrounding.getY() != previous.getY()) && !reached) {
 					//FLAW 2: Every surrounding square that is colored is considered an intersection.
-					//What we really want is only intersections not associated with endpoints (unintentional intersections)
 					//POSSIBLE FIX: Any perceived intersection (a surrounding square colored) must not be from the previous two nodes (two lines can't intersect at more than once place)
 					intersections.add(new Intersection(x, y));
 					reached = true;
 					counter = 0;
 				}
-				//requires a distance of 4 between intersecting points along a single line segment (street node connector)
-
 				if(counter > 3)
 					reached = false;
 				//prevents line from being "broken" when undo button is pressed for intersecting lines
@@ -423,9 +369,7 @@ public class DrawCity extends JFrame {
 		}
 		@Override
 		public void mouseDragged(MouseEvent e) {
-			// TODO Auto-generated method stub
 			end = new Vec2(e.getX(), e.getY());
-			//"plus" mitigates problem that mouseDragged is called many times before repaint runs (causing unintended skipping of space)
 			shift = shift.plus(end.minus(curLoc));
 			curLoc = end;
 			this.repaint();
@@ -433,14 +377,12 @@ public class DrawCity extends JFrame {
 
 		@Override
 		public void mouseMoved(MouseEvent e) {
-			// TODO Auto-generated method stub
 			curLoc = new Vec2(e.getX(), e.getY());
 			this.repaint();
 		}
 
 		@Override
 		public void mouseClicked(MouseEvent e) {
-			// TODO Auto-generated method stub		
 			curLoc = new Vec2(e.getX(), e.getY());
 			if(!isFirstClicked)
 				isFirstClicked = true;
@@ -474,13 +416,8 @@ public class DrawCity extends JFrame {
 
 		@Override
 		public void mouseWheelMoved(MouseWheelEvent e) {
-			//Will most often be either -1.0 or 1.0
 			double rotation = e.getPreciseWheelRotation();
-			
 			scrollLoc = new Vec2(e.getX(), e.getY());
-			
-
-			//zoomFactor = Math.max(0, zoomFactor-rotation*0.5);
 			zoomFactor = -rotation;
 			this.repaint();
 
@@ -503,7 +440,6 @@ public class DrawCity extends JFrame {
 			}
 		});
 		
-		//new DrawCity();
 	}
 
 }
